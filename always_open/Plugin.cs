e7f64a8b83a7881e58ed3f6d949aaa3323f9c2b7
devopsdinosaur <devopsdinosaur@gmail.com>
2023-01-09 08:57:08 -0500

    Added always_open mod

---
+++ always_open/Plugin.cs
@@ -0,0 +1,31 @@
+﻿
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Wish;
+
+
+[BepInPlugin("devopsdinosaur.sunhaven.debugging", "DEBUGGING", "0.0.1")]
+public class Plugin : BaseUnityPlugin {
+
+	private Harmony m_harmony = new Harmony("devopsdinosaur.sunhaven.debugging");
+	public static ManualLogSource logger;
+
+
+	public Plugin() {
+	}
+
+	private void Awake() {
+		Plugin.logger = this.Logger;
+		logger.LogInfo((object) "devopsdinosaur.sunhaven.debugging v0.0.1 loaded.");
+		this.m_harmony.PatchAll();
+	}
+
+	[HarmonyPatch(typeof(ScenePortalSpot), "Awake")]
+	class HarmonyPatch_ScenePortalSpot_Awake {
+
+		private static void Postfix(ref ScenePortalSpot __instance, ref bool ___hasOpenAndCloseTime) {
+			___hasOpenAndCloseTime = false;
+		}
+	}
+}
---
+++ debugging/Plugin.cs
@@ -0,0 +1,55 @@
+﻿
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Wish;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Reflection;
+
+
+[BepInPlugin("devopsdinosaur.sunhaven.debugging", "DEBUGGING", "0.0.1")]
+public class Plugin : BaseUnityPlugin {
+
+	private Harmony m_harmony = new Harmony("devopsdinosaur.sunhaven.debugging");
+	public static ManualLogSource logger;
+
+
+	public Plugin() {
+	}
+
+	private void Awake() {
+		Plugin.logger = this.Logger;
+		logger.LogInfo((object) "devopsdinosaur.sunhaven.debugging v0.0.1 loaded.");
+		this.m_harmony.PatchAll();
+	}
+
+	/*
+	[HarmonyPatch(typeof(NPCManager), "FixedUpdate")]
+	class HarmonyPatch_NPCManager_Update {
+
+		const float CHECK_FREQUENCY = 1.0f;
+		static float m_elapsed = 0f;
+		static bool m_done = false;
+
+		private static bool Prefix(ref NPCManager __instance, bool ___initialized, Dictionary<string, NPCAI> ____npcs) {
+			if ((m_elapsed += Time.fixedDeltaTime) < CHECK_FREQUENCY) {
+				return true;
+			}
+			m_elapsed = 0f;
+			if (SceneManager.sceneCount < 2 || !___initialized || GameManager.ApplicationQuitting) {
+				return true;
+			}
+			if (m_done) {
+				return true;
+			}
+			foreach (NPCAI npc in ____npcs.Values) {
+				logger.LogInfo(npc.ActualNPCName);
+			}
+			m_done = true;
+			return true;
+		}
+	}
+	*/
+}
