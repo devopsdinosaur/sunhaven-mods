b77c7b0745ed1695d503ffaafde53bb029cf7720
devopsdinosaur <devopsdinosaur@gmail.com>
2023-04-27 22:51:42 -0400

    ---

--- expanded_storage/ExpandedStoragePlugin.cs
+++ expanded_storage/ExpandedStoragePlugin.cs
@@ -8,6 +8,7 @@ using System;
 using System.Reflection;
 using UnityEngine.Events;
 using System.Collections.Generic;
+using TMPro;
 
 
 [BepInPlugin("devopsdinosaur.sunhaven.expanded_storage", "Expanded Storage", "0.0.1")]
@@ -20,24 +21,11 @@ public class ExpandedStoragePlugin : BaseUnityPlugin {
 
 	private const int CHEST_ORIGINAL_SLOT_COUNT = 30;
 
-	private const int ORIGINAL_SLOT_COL_COUNT = 8;
-	private const int ORIGINAL_SLOT_ROW_COUNT = 5;
-	private const int ORIGINAL_SLOT_TOTAL_COUNT = ORIGINAL_SLOT_COL_COUNT * ORIGINAL_SLOT_ROW_COUNT;
-	private const int SPACE_X = 4;
-	private const int SPACE_Y = 4;
-	private const float STARTING_SCALE = 1f;
-	private const float SCALE_DEC = 0.05f;
-	private const int MIN_SLOT_WIDTH = 4;
-
-	// this will be true if the scaling algorithm is unable to fit requested slots
-	private static bool m_temporary_disable = false;
-
 	private void Awake() {
 		logger = this.Logger;
 		try {
 			m_enabled = this.Config.Bind<bool>("General", "Enabled", true, "Set to false to disable this mod.");
 			m_num_chest_slots = this.Config.Bind<int>("General", "Chest Slot Count", 100, "Number of chest inventory slots");
-			m_temporary_disable = false;
 			if (m_enabled.Value) {
 				this.m_harmony.PatchAll();
 			}
@@ -87,99 +75,70 @@ public class ExpandedStoragePlugin : BaseUnityPlugin {
 	}
 
 	[HarmonyPatch(typeof(Inventory), "Start")]
-	class HarmonyPatch_Chest_LoadInventory {
+	class HarmonyPatch_Inventory_Start {
 
-		private static bool Prefix(Inventory __instance, Transform ____inventoryPanel) {
+		private static void Postfix(Inventory __instance, Transform ____inventoryPanel) {
 			try {
-				if (!m_enabled.Value) {
+				GameObject sort_button = null;
+				GameObject backpack_title = null;
+
+				bool __enum_descendants_callback_find_sort_button__(Transform transform) {
+					if (sort_button == null && transform.name == "SortChestButton") {
+						sort_button = transform.gameObject; 
+					} else if (backpack_title == null && transform.name == "Text (TMP)") {
+						backpack_title = transform.gameObject;
+					}
 					return true;
+				}
+
+				GameObject create_navigation_button(GameObject left_obj, string name, string text, Vector3 direction) {
+					GameObject obj = new GameObject(name);
+					obj.transform.SetParent(sort_button.transform.parent);
+					TextMeshProUGUI tmp = obj.AddComponent<TextMeshProUGUI>();
+					RectTransform other_rect = left_obj.GetComponent<RectTransform>();
+					RectTransform obj_rect = tmp.rectTransform;
+					UnityEngine.UI.Button button = obj.AddComponent<UnityEngine.UI.Button>();
+					tmp.fontWeight = FontWeight.Bold;
+					tmp.color = Color.black;
+					tmp.text = text;
+					//obj.transform.Rotate(0, 0, 90f);
+					obj_rect.localPosition = other_rect.localPosition + Vector3.right * (other_rect.rect.width * 2);
+					button.onClick.AddListener(new UnityAction(delegate {
+						logger.LogInfo("hello there!");
+					}));
+					return obj;
+				}
+
+				if (!m_enabled.Value) {
+					return;
 				}
 				Slot[] original_slots = ____inventoryPanel.GetComponentsInChildren<Slot>(includeInactive: true);
 				if (original_slots.Length != CHEST_ORIGINAL_SLOT_COUNT) {
-					return true;
+					return;
 				}
 				Slot slot0 = original_slots[0];
 				__instance.maxSlots = m_num_chest_slots.Value;
 				for (int index = original_slots.Length; index < __instance.maxSlots; index++) {
 					GameObject.Instantiate(slot0.gameObject, slot0.transform.parent);
 				}
-				RectTransform inventory_rect = ____inventoryPanel.GetComponent<RectTransform>();
-				float scale = STARTING_SCALE;
-				int slot_index = 0;
-				int panel_width = (int) inventory_rect.rect.width;
-				int panel_height = (int) inventory_rect.rect.height;
-				int slot_width = -1;
-				int slot_height = -1;
-				int x = -1;
-				int y = -1;
-			
-				bool find_slots(Transform transform) {
-					Slot slot = transform.GetComponent<Slot>();
-					if (slot == null) {
-						return true;
-					}
-					//if (slot_index >= ORIGINAL_SLOT_TOTAL_COUNT) {
-					//	return false;
-					//}
-					// RectTransform, UI.Image, NavigationElement, CanvasRenderer
-					//list_component_types(transform);
-					RectTransform rect = transform.GetComponent<RectTransform>();
-					NavigationElement nav = transform.GetComponent<NavigationElement>();
-					if (slot_index == 0) {
-						scale = STARTING_SCALE;
-						for (;;) {
-							bool does_fit = true;
-							slot_width = (int) (rect.rect.width * scale);
-							slot_height = (int) (rect.rect.height * scale);
-							if (slot_width < MIN_SLOT_WIDTH) {
-								logger.LogError("** ERROR - unable to fit requested number of inventory slots due to panel space constraints (slot width would be less than minimum width of " + MIN_SLOT_WIDTH + ").");
-								m_temporary_disable = true;
-								return false;
-							}
-							//logger.LogInfo("panel_width: " + panel_width + ", panel_height: " + panel_height);
-							//logger.LogInfo("orig_slot_width: " + rect.rect.width + ", orig_slot_height: " + rect.rect.height);
-							//logger.LogInfo("scale: " + scale);
-							//logger.LogInfo("slot_width: " + slot_width + ", slot_height: " + slot_height);
-							x = 0;
-							y = 0;
-							for (int check_index = 0; check_index < m_num_chest_slots.Value; check_index++) {
-								//logger.LogInfo("slot" + check_index + " - x: " + x + ", y: " + y);
-								if ((x += slot_width + SPACE_X) >= panel_width) {
-									x = 0;
-									if ((y += slot_height + SPACE_Y) + slot_height + SPACE_Y >= panel_height) {
-										does_fit = false;
-										break;
-									}
-								}
-							}
-							//logger.LogInfo("does_fit: " + does_fit);
-							if (does_fit) {
-								break;
-							}
-							scale -= SCALE_DEC;
-						}
-						x = 0;
-						y = 0;
-					}
-					//logger.LogInfo("slot" + slot_index + " - x: " + x + ", y: " + y);
-					rect.localScale = new Vector3(scale, scale);
-					rect.localPosition = new Vector3(0, 0);
-					//m_slot_positions[= new Vector3(inventory_rect.rect.x + x, inventory_rect.rect.y + y);
-					if ((x += slot_width + SPACE_X) >= panel_width) {
-						x = 0;
-						y += slot_height + SPACE_Y;
-					}
-				
-					slot_index++;
-					return true;
-				}
-			
-				list_descendants(____inventoryPanel, find_slots, 0);
-				return true;
+				enum_descendants(____inventoryPanel.parent.parent.parent, __enum_descendants_callback_find_sort_button__);
+				logger.LogInfo((backpack_title == null ? "null" : "not null"));
+				create_navigation_button(
+					create_navigation_button(
+						sort_button,
+						"chest_page_button_up", 
+						"up", 
+						Vector3.up
+					),
+					"chest_page_button_down",
+					"down",
+					Vector3.down
+				);
+				//create_navigation_button("->", 1);
+				return;
 			} catch (Exception e) {
-				logger.LogError("** Inventory.Start_Prefix ERROR - " + e);
+				logger.LogError("** Inventory_Start_Postfix ERROR - " + e);
 			}
-			return true;
 		}
 	}
 
