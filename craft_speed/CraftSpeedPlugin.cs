5dcde6470142161d5d464c70f42d3bbfd84fc36f
devopsdinosaur <devopsdinosaur@gmail.com>
2023-11-24 20:37:29 -0500

    [craft_speed v0.0.5] Added BeehiveBox support

--- craft_speed/CraftSpeedPlugin.cs
+++ craft_speed/CraftSpeedPlugin.cs
@@ -4,9 +4,12 @@ using BepInEx.Configuration;
 using HarmonyLib;
 using Wish;
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
 
 
-[BepInPlugin("devopsdinosaur.sunhaven.craft_speed", "Craft Speed", "0.0.4")]
+[BepInPlugin("devopsdinosaur.sunhaven.craft_speed", "Craft Speed", "0.0.5")]
 public class CraftSpeedPlugin : BaseUnityPlugin {
 
 	private Harmony m_harmony = new Harmony("devopsdinosaur.sunhaven.craft_speed");
@@ -29,6 +32,7 @@ public class CraftSpeedPlugin : BaseUnityPlugin {
 		"Anvil", 
 		"Bakers Station",
 		"Basic Furniture Table", 
+		"BeeHiveBox",
 		"Composter Table", 
 		"Construction Table", 
 		"Cooking Pot", 
@@ -73,6 +77,8 @@ public class CraftSpeedPlugin : BaseUnityPlugin {
 	};
 	private static ConfigEntry<bool>[] m_table_enabled = new ConfigEntry<bool>[m_table_names.Length];
 	private static ConfigEntry<float>[] m_table_speeds = new ConfigEntry<float>[m_table_names.Length];
+	private static int m_beebox_index = -1;
+	private static Dictionary<int, float> m_honey_times = new Dictionary<int, float>();
 
 	private void Awake() {
 		logger = this.Logger;
@@ -80,6 +86,9 @@ public class CraftSpeedPlugin : BaseUnityPlugin {
 			m_enabled = this.Config.Bind<bool>("General", "Enabled", true, "Set to false to disable this mod.");
 			m_craft_speed = this.Config.Bind<float>("General", "Craft Speed Multiplier", 10f, "Speed multiplier for item crafting (float, 1 = game default (1.2 for humans) [note: this stomps the human 20% passive; should not affect anything else])");
 			for (int index = 0; index < m_table_names.Length; index++) {
+				if (m_table_names[index] == "BeeHiveBox") {
+					m_beebox_index = index;
+				}
 				m_table_enabled[index] = this.Config.Bind<bool>("General", m_table_names[index] + " Enabled", true, "If true then the '" + m_table_names[index] + "' table will use the craft speed multiplier; if false then it will use the game default speed.");
 				m_table_speeds[index] = this.Config.Bind<float>("General", m_table_names[index] + " Speed Multiplier", 0f, "If this value is non-zero and '" + m_table_names[index] + " Enabled' is true then this will be the craft speed multiplier used for the '" + m_table_names[index] + "' table (overriding the global one).  If this value is 0 then the global multiplier will be used (if table is enabled).");
 			}
@@ -115,6 +124,29 @@ public class CraftSpeedPlugin : BaseUnityPlugin {
 			} catch (Exception e) {
 				logger.LogError("** CraftingTable.Awake_Postfix ERROR - " + e);
 			}
+		}
+	}
+
+	[HarmonyPatch(typeof(BeeHiveBox), "LateUpdate")]
+	class HarmonyPatch_BeeHiveBox_LateUpdate {
+
+		const float CHECK_FREQUENCY = 1.0f;
+		static float m_elapsed = CHECK_FREQUENCY;
+
+		private static bool Prefix(BeeHiveBox __instance, CraftingTableData ___craftingData) {
+			try {
+				if (!m_enabled.Value || (m_elapsed += Time.fixedDeltaTime) < CHECK_FREQUENCY) {
+					return true;
+				}
+				m_elapsed = 0f;
+				if (!m_honey_times.ContainsKey(___craftingData.items[0].item.ID())) {
+					m_honey_times[___craftingData.items[0].item.ID()] = ___craftingData.items[0].craftTime;
+				}
+				___craftingData.items[0].craftTime = m_honey_times[___craftingData.items[0].item.ID()] / (m_table_speeds[m_beebox_index].Value > 0f ? m_table_speeds[m_beebox_index].Value : m_craft_speed.Value);
+			} catch (Exception e) {
+				logger.LogError("** HarmonyPatch_BeeHiveBox_LateUpdate_Prefix ERROR - " + e);
+			}
+			return true;
 		}
 	}
 }
