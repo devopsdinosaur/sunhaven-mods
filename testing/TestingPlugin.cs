a6e73143cc87910d8661dc2a02c2bf530e995da3
devopsdinosaur <devopsdinosaur@gmail.com>
2024-08-02 08:30:09 -0400

    [clingy_npcs v0.0.3] Added npc summon function

--- testing/TestingPlugin.cs
+++ testing/TestingPlugin.cs
@@ -168,6 +168,7 @@ public class TestingPlugin : BaseUnityPlugin {
         }
     }
 
+	/*
 	class NpcSummoner : MonoBehaviour {
 		
 		private NPCAI m_npc = null;
@@ -176,59 +177,107 @@ public class TestingPlugin : BaseUnityPlugin {
 		private Vector3 m_prev_pos;
 		private AIState m_prev_ai_state;
 		private Direction m_prev_face_direction;
+		private float m_update_elapsed = 0;
 
         private void Awake() {
 			this.m_npc = this.gameObject.GetComponent<NPCAI>();
 		}
 
         public static void summon_npc(string name, Player player) {
-			foreach (NPCAI npc in Resources.FindObjectsOfTypeAll(typeof(NPCAI))) {
-                if (npc.ActualNPCName != name) {
-                    continue;
-                }
-				NpcSummoner summoner = npc.gameObject.GetComponent<NpcSummoner>();
-				if (summoner == null) {
-					summoner = npc.gameObject.AddComponent<NpcSummoner>();
+			try {
+				foreach (NPCAI npc in Resources.FindObjectsOfTypeAll(typeof(NPCAI))) {
+					if (npc.ActualNPCName != name) {
+						continue;
+					}
+					NpcSummoner summoner = npc.gameObject.GetComponent<NpcSummoner>();
+					if (summoner == null) {
+						summoner = npc.gameObject.AddComponent<NpcSummoner>();
+					}
+					summoner.summon(player);
+					break;
 				}
-				summoner.summon(player);
-                break;
-            }
-        }
+			} catch (Exception e) {
+				logger.LogError("** NpcSummoner.summon_npc ERROR - " + e);
+			}
+		}
 
         private void summon(Player player) {
-            const float OFFSET = 2f;
-			if (!m_is_summoned) {
-				this.m_is_summoned = true;
-				this.m_prev_ai_state = this.m_npc.AIState;
-				this.m_prev_scene = this.m_npc.Scene;
-				this.m_prev_pos = this.m_npc.transform.position;
-				this.m_prev_face_direction = m_npc.FacingDirection;
-			}
-            this.m_npc.Scene = ScenePortalManager.ActiveSceneName;
-            this.m_npc.SetAIState(AIState.Still);
-            switch (player.facingDirection) {
-            case Direction.North:
-				this.m_npc.transform.position = player.transform.position + Vector3.up * OFFSET;
-				this.m_npc.FacingDirection = Direction.South;
-				break;
-            case Direction.South:
-				this.m_npc.transform.position = player.transform.position + Vector3.down * OFFSET;
-				this.m_npc.FacingDirection = Direction.North;
-				break;
-            case Direction.East:
-				this.m_npc.transform.position = player.transform.position + Vector3.right * OFFSET;
-				this.m_npc.FacingDirection = Direction.West;
-				break;
-            case Direction.West:
-				this.m_npc.transform.position = player.transform.position + Vector3.left * OFFSET;
-				this.m_npc.FacingDirection = Direction.East;
-				break;
+			try {
+				const float OFFSET = 2f;
+				if (!m_is_summoned) {
+					this.m_is_summoned = true;
+					this.m_prev_ai_state = this.m_npc.AIState;
+					this.m_prev_scene = this.m_npc.Scene;
+					this.m_prev_pos = this.m_npc.transform.position;
+					this.m_prev_face_direction = m_npc.FacingDirection;
+				}
+				this.m_npc.Scene = ScenePortalManager.ActiveSceneName;
+				this.m_npc.SetAIState(AIState.Still);
+				switch (player.facingDirection) {
+				case Direction.North:
+					this.m_npc.transform.position = player.transform.position + Vector3.up * OFFSET;
+					this.m_npc.FacingDirection = Direction.South;
+					break;
+				case Direction.South:
+					this.m_npc.transform.position = player.transform.position + Vector3.down * OFFSET;
+					this.m_npc.FacingDirection = Direction.North;
+					break;
+				case Direction.East:
+					this.m_npc.transform.position = player.transform.position + Vector3.right * OFFSET;
+					this.m_npc.FacingDirection = Direction.West;
+					break;
+				case Direction.West:
+					this.m_npc.transform.position = player.transform.position + Vector3.left * OFFSET;
+					this.m_npc.FacingDirection = Direction.East;
+					break;
+				}
+				this.m_npc.gameObject.SetActive(true);
+			} catch (Exception e) {
+                logger.LogError("** NpcSummoner.summon ERROR - " + e);
             }
-            this.m_npc.gameObject.SetActive(true);
-        }
+}
+
+		private void Update() {
+			try {
+				if (!this.m_is_summoned || (this.m_update_elapsed += Time.deltaTime) < 1) {
+					return;
+				}
+				this.m_update_elapsed = 0;
+				if (ScenePortalManager.ActiveSceneName == this.m_npc.Scene) {
+					return;
+				}
+				//debug_log($"NpcSummoner.Update - player left scene (active scene: {ScenePortalManager.ActiveSceneName}, npc scene: {this.m_npc.Scene}; returning {this.m_npc.ActualNPCName} to previous scene: {this.m_prev_scene}, pos: {this.m_prev_pos}, state: {this.m_prev_ai_state}");
+				this.m_npc.Scene = this.m_prev_scene;
+				this.m_npc.transform.position = this.m_prev_pos;
+				this.m_npc.FacingDirection = this.m_prev_face_direction;
+				this.m_npc.SetAIState(this.m_prev_ai_state);
+				this.m_is_summoned = false;
+			} catch (Exception e) {
+				logger.LogError("** NpcSummoner.Update ERROR - " + e);
+			}
+		}
 	}
 
-    [HarmonyPatch(typeof(Player), "Update")]
+	[HarmonyPatch(typeof(Relationships), "OnEnable")]
+	class HarmonyPatch_Relationships_OnEnable {
+
+		private static void Postfix(Relationships __instance, Dictionary<string, RelationshipPanel> ___relationshipPanels) {
+			try {
+				foreach (KeyValuePair<string, RelationshipPanel> item in ___relationshipPanels) {
+					item.Value.npcImage.gameObject.AddComponent<UnityEngine.UI.Button>().onClick.AddListener(new UnityAction(delegate() {
+						NpcSummoner.summon_npc(item.Key, Player.Instance);
+						UIHandler.Instance.CloseInventory();
+					}));
+				}
+			} catch (Exception e) {
+				logger.LogError("** HarmonyPatch_Relationships_OnEnable.Postfix ERROR - " + e);
+			}
+		}
+	}
+	*/
+
+	/*
+	[HarmonyPatch(typeof(Player), "Update")]
     class HarmonyPatch_Player_Update {
 
         private static void Postfix(Player __instance) {
@@ -236,12 +285,13 @@ public class TestingPlugin : BaseUnityPlugin {
 				if (!__instance.IsOwner || !Input.GetKeyDown(KeyCode.Backspace)) {
 					return;
 				}
-				summon_npc("Anne", __instance);
+				NpcSummoner.summon_npc("Anne", __instance);
             } catch (Exception e) {
                 logger.LogError("** XXXXX.Prefix ERROR - " + e);
             }
         }
     }
+	*/
 
 	[HarmonyPatch(typeof(DialogueController), "Awake")]
 	class HarmonyPatch_DialogueController_Awake {
