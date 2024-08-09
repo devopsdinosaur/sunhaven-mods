fbd51471eb05c435e48b8b7552a2fab5960d8c9f
devopsdinosaur <devopsdinosaur@gmail.com>
2024-08-09 07:13:03 -0400

    ---

--- self_portrait/SelfPortraitPlugin.cs
+++ self_portrait/SelfPortraitPlugin.cs
@@ -33,8 +33,8 @@ public class SoundManagerPlugin : BaseUnityPlugin {
 		logger = this.Logger;
 		try {
 			m_enabled = this.Config.Bind<bool>("General", "Enabled", true, "Set to false to disable this mod.");
-			m_subdir = this.Config.Bind<string>("General", "Subfolder", "self_portrait", "Subfolder under 'plugins' in which per-user self portrait folders will be located.");
-			m_default_username = this.Config.Bind<string>("General", "Default Username", "default", "Fallback self portrait directory to use if there is none for current user.");
+			m_subdir = this.Config.Bind<string>("General", "Subfolder", "self_portrait", "Subfolder under 'plugins' in which per-user self portrait folders will be located (default: 'self_portrait').");
+			m_default_username = this.Config.Bind<string>("General", "Default Username", "default", "Fallback self portrait directory to use if there is none for current user (default: default).");
             m_hotkey_modifier = this.Config.Bind<string>("General", "Hotkey Modifier", "LeftControl,RightControl", "Comma-separated list of Unity Keycodes used as the special modifier key (i.e. ctrl,alt,command) one of which is required to be down for hotkeys to work.  Set to '' (blank string) to not require a special key (not recommended).  See this link for valid Unity KeyCode strings (https://docs.unity3d.com/ScriptReference/KeyCode.html)");
             m_hotkey_reload = this.Config.Bind<string>("General", "Reload Hotkey", "F2", "Comma-separated list of Unity Keycodes, any of which (in combination with modifier key [if not blank]) will reload portrait images.  See this link for valid Unity KeyCode strings (https://docs.unity3d.com/ScriptReference/KeyCode.html)");
             m_force_outfit = this.Config.Bind<string>("General", "Force Outfit", "", "Specify one of (Summer, Fall, Winter, Wedding, or Swimsuit) to override the game logic for portrait outfit selection for self and NPC (set to empty or invalid string to disable this setting).");
