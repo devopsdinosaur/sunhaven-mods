c45daba913d558a443cf016fd4cae0163567b729
devopsdinosaur <devopsdinosaur@gmail.com>
2025-03-04 16:38:39 -0500

    [no_logo v0.0.2]

--- no_logo/NoLogoPlugin.cs
+++ no_logo/NoLogoPlugin.cs
@@ -7,6 +7,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
 using Wish;
 
@@ -51,8 +53,8 @@ public class NoLogoPlugin : DDPlugin {
         }
     }
 
-	[HarmonyPatch(typeof(GameLoader), "LateUpdate")]
-	class HarmonyPatch_GameLoader_LateUpdate {
+	[HarmonyPatch(typeof(MainMenuLoader), "Start")]
+	class HarmonyPatch_MainMenuLoader_Start {
 
 		class QuickLoader : MonoBehaviour {
 			private static QuickLoader m_instance;
@@ -61,9 +63,9 @@ public class NoLogoPlugin : DDPlugin {
 					return m_instance;
 				}
 			}
-			private GameLoader m_loader;
+			private MainMenuLoader m_loader;
 
-			public static QuickLoader create(GameLoader loader) {
+			public static QuickLoader create(MainMenuLoader loader) {
 				if (m_instance != null) {
 					return m_instance;
 				}
@@ -80,8 +82,8 @@ public class NoLogoPlugin : DDPlugin {
 				for (;;) {
 					bool found = false;
 					foreach (GameObject obj in SceneManager.GetActiveScene().GetRootGameObjects()) {
-						if (obj.name == "UI_SplashScreen") {
-							obj.SetActive(false);
+						if (obj.name == "UI_Loading") {
+							obj.transform.Find("Animation").gameObject.SetActive(false);
 							found = true;
 							break;
 						}
@@ -91,36 +93,47 @@ public class NoLogoPlugin : DDPlugin {
 					}
 					yield return new WaitForSeconds(0.1f);
 				}
-				
-				/*
-				foreach (GameObject obj in SceneManager.GetActiveScene().GetRootGameObjects()) {
-					if (obj.name == "UI_SplashScreen") {
-						obj.SetActive(false);
-						break;
+				int _assetsToPreloadCount = (int) ReflectionUtils.get_field_value(this.m_loader, "_assetsToPreloadCount");
+				MethodInfo UpdateProgressDuringStep = ReflectionUtils.get_method(this.m_loader, "UpdateProgressDuringStep");
+				if (_assetsToPreloadCount > 0) {
+					AssetReference[] array = (AssetReference[]) ReflectionUtils.get_field_value(this.m_loader, "assetReferencesToPreload");
+					int _assetsPreloadedCount = 0;
+					List<ClothingLayerSprites> _preloadedAssets = (List<ClothingLayerSprites>) ReflectionUtils.get_field_value(this.m_loader, "_preloadedAssets");
+					for (int i = 0; i < array.Length; i++) {
+						AsyncOperationHandle<ClothingLayerSprites> asyncOperationHandle = array[i].LoadAssetAsync<ClothingLayerSprites>();
+						asyncOperationHandle.Completed += new Action<AsyncOperationHandle<ClothingLayerSprites>>(delegate (AsyncOperationHandle<ClothingLayerSprites> operationHandle) {
+							if (operationHandle.Status == AsyncOperationStatus.Succeeded) {
+								_preloadedAssets.Add(operationHandle.Result);
+							}
+							_assetsPreloadedCount++;
+						});
+					}
+					while (_assetsPreloadedCount < _assetsToPreloadCount) {
+						float stepProgress = (float) _assetsPreloadedCount / (float) _assetsToPreloadCount;
+						UpdateProgressDuringStep.Invoke(this.m_loader, new object[] { stepProgress, 0f, 0.2f });
+						yield return null;
 					}
 				}
-				AsyncOperation loadOperation = SceneManager.LoadSceneAsync("Content", LoadSceneMode.Additive);
-				yield return new WaitUntil(() => loadOperation.isDone);
-				GameObject[] rootGameObjects = SceneManager.GetSceneByName("Content").GetRootGameObjects();
-				MasterScriptableObjectContainer masterScriptableObjectContainer = null;
-				GameObject[] array = rootGameObjects;
-				for (int i = 0; i < array.Length; i++) {
-					masterScriptableObjectContainer = array[i].GetComponent<MasterScriptableObjectContainer>();
-					if (masterScriptableObjectContainer != null) {
-						break;
+				ReflectionUtils.get_field(this.m_loader, "_currentProgress").SetValue(this.m_loader, 0.2f);
+				AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+				operation.allowSceneActivation = false;
+				while (!operation.isDone) {
+					float stepProgress = Mathf.Clamp01(operation.progress / 0.9f);
+					UpdateProgressDuringStep.Invoke(this.m_loader, new object[] { stepProgress, 0.2f, 0.8f });
+					if (operation.progress >= 0.8999f) {
+						ReflectionUtils.get_field(this.m_loader, "_currentProgress").SetValue(this.m_loader, 1f);
+						ReflectionUtils.get_field(this.m_loader, "_displayProgress").SetValue(this.m_loader, 1f);
+						((UnityEngine.UI.Image) ReflectionUtils.get_field_value(this.m_loader, "fill")).fillAmount = 1f;
+						yield return new WaitForSeconds(0.25f);
+						operation.allowSceneActivation = true;
 					}
+					yield return null;
 				}
-				if (masterScriptableObjectContainer != null) {
-					this.m_loader.GetType().GetField("masterContent", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(this.m_loader, masterScriptableObjectContainer.masterContent);
-				}
-				AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync("Content");
-				yield return new WaitUntil(() => unloadOperation.isDone);
-				this.m_loader.LoadLevel(1);
-				*/
+				Application.runInBackground = GameManager.Multiplayer || Wish.Settings.RunInBackground;
 			}
 		}
 
-		private static bool Prefix(GameLoader __instance) {
+		private static bool Prefix(MainMenuLoader __instance) {
 			try {
 				if (!Settings.m_enabled.Value) {
 					return true;
@@ -129,7 +142,7 @@ public class NoLogoPlugin : DDPlugin {
 				QuickLoader.Instance.quick_load();
 				return false;
 			} catch (Exception e) {
-				logger.LogError("** HarmonyPatch_LoadMainMenu_LoadMasterContent.Prefix ERROR - " + e);
+				logger.LogError("** HarmonyPatch_MainMenuLoader_Start.Prefix ERROR - " + e);
 			}
 			return true;
 		}
--- no_logo/changelog.md
+++ no_logo/changelog.md
@@ -1,2 +1,5 @@
-﻿# 0.0.1
+﻿# 0.0.2
+- Updated to work with game v1.7.5
+
+# 0.0.1
 - Initial release
